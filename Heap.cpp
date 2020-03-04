2d3526b795cd96834834441d4559f7c6ff780efd
Mark Chesney <mchesney@seattleu.edu>
2020-03-04 13:07:10 -0800

    create Heap.h and Heap.cpp files

---
+++ Heap.cpp
@@ -0,0 +1,9 @@
+// Implementation file for the enhanced heap class
+// Created by Mark Chesney, March 2020.
+#include "Heap.h"
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+// Constructor
