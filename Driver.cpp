cb1fcca53f672918f2056896624425ad52d80b42
Mark Chesney <mchesney@seattleu.edu>
2020-03-17 17:19:07 -0700

    initialize data structures (before while-loop)

--- Driver.cpp
+++ Driver.cpp
@@ -1,9 +1,12 @@
 // Graphing Algorithms (Dijkstra's, Prim's)
 // Data structures: Priority Queues / Heaps
 #include <iostream>
+#include <set>
 #include "Heap.h"
 #include "Graph.h"
 using namespace std;
+
+void dijkstra(Graph, string);
 
 int main() {
     cout << "running Driver.cpp file" << endl;
@@ -29,5 +32,65 @@ int main() {
 
     }
 
+    // Dijkstra's Algorithm: solving single-source shortest paths problem
+    dijkstra(g, "a");
+
     return 0;
+}
+
+
+void dijkstra(Graph g, string start) {
+    cout << "Dijkstra's Algorithm" << endl;
+    vector<string> vertices = g.vertices();
+    int n = vertices.size();     // n is |V|, number of vertices in the graph
+
+	// data structures
+    map<string, string> path;
+    map<string, int> shortest;
+	set<string> found;
+    // map<string, EdgeList> path;
+
+    for (auto itr : vertices) {
+        path.insert(pair<string, string>("abc", "temp_placeholder"));
+        shortest.insert(pair<string, int>("abc", 0));
+    }
+    // shortest[start] = 0
+    shortest.find("abc")->second = 0;
+
+    cout << "temp line" << n << path.size() << shortest.size() << found.size() << endl;
+
+    // unsolved = Heap(shortest)
+    //
+    // # get shortest path to every other vertex
+    // while len(found) < n:
+    //     # find closest unsolved vertex
+    //     closest, distance = unsolved.dequeue()
+    //
+    //     # closest vertex is now solved, we know that no other shorter path exists
+    //     found.add(closest)
+    //
+    //     # update paths to other vertices via closest
+    //     for k in g[closest]:
+    //         if k not in found:
+    //             new_path_to_k = shortest[closest] + g[closest][k]
+    //             if new_path_to_k < unsolved.weights[k]:
+    //                 unsolved.enqueue(k, new_path_to_k)  # fix weight in priority queue
+    //                 shortest[k] = new_path_to_k
+    //                 path[k] = closest
+    //
+    // # report results (not officially part of Dijkstra's--we wouldn't count this in time analysis)
+    // paths = {}
+    // for v in g:
+    //     if v != start:
+    //         this_path = []  # collect the previously-found shortest path edges from start to v
+    //         w = v
+    //         while w != start:
+    //             this_path.append(w)  # w is previous vertex in shortest path
+    //             w = path[w]
+    //         this_path.append(start)
+    //         this_path.reverse()
+    //         paths[v] = ''.join(this_path)
+    // del (shortest[start])  # take out shortest[start] (which is zero)
+    // return shortest, paths
+
 }
